9da0304518751528eb375151607d2b784192be87
ethanmah <ett.mah@gmail.com>
2023-12-06 18:39:35 -0500

    Main Branch Starting Point

---
+++ TCAC_Fantasy/requests/constant.py
@@ -0,0 +1,5 @@
+FANTASY_BASE_ENDPOINT = 'https://fantasy.espn.com/apis/v3/games/'
+
+FANTASY_SPORTS = {
+    'nba' : 'fba'
+}
---
+++ checkAPIConnection.py
@@ -0,0 +1,10 @@
+# Basketball API
+from TCAC_Fantasy.basketball import League
+
+# Initiate League Info
+league = League(league_id=467491942, year=2024)
+
+print("Testing Successful")
+print(league.league_id, league.year)
+
+# TODO: Clean ReadMe and MetaData
